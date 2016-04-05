7835d63101563aa9a5208516a47d6130743be1f7
Stefan Fischer <sfischer13@ymail.com>
2016-04-05 23:39:18 +0200

    Add more history settings

--- sensible.zsh
+++ sensible.zsh
@@ -5,5 +5,9 @@ setopt no_beep
 setopt auto_cd
 
 # history
+export HISTSIZE=10000
+export SAVEHIST=10000
+setopt append_history
 setopt hist_ignore_all_dups
 setopt hist_ignore_space
+setopt share_history
